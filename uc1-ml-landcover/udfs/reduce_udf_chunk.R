dca0cde6b52c3066c25eb94d9f948717ed56c3bd
basil tufail <btufail@eurac.edu>
2022-09-27 07:41:53 +0000

    reduce_dimension_udf_chunk

---
+++ uc1-ml-landcover/udfs/reduce_udf_chunk.R
@@ -0,0 +1,20 @@
+getModel = NULL
+
+udf_setup = function(context) {
+  url1      <- 'https://github.com/Open-EO/openeo-udf-python-to-r/blob/UC1_ML/models/TestModel1.rds?raw=True'
+  getModel  <<- readRDS(gzcon(url(url1)))
+}
+
+
+
+udf_chunked = function(data,context){
+  names(data)<-caret::predictors(getModel)
+  data = data.frame(t(data))
+  prediction <-predict(getModel, data)
+  prediction = as.numeric(prediction)
+  return(prediction)
+}
+
+
+
+
