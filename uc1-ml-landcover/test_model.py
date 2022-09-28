96476a8096e25bb5e8777c8994f2241f90dd7f35
Basil Tufail <44399454+basu26@users.noreply.github.com>
2022-09-28 17:13:51 +0200

    Update test_model.py

--- uc1-ml-landcover/test_model.py
+++ uc1-ml-landcover/test_model.py
@@ -1,9 +1,6 @@
-#from udf_lib import execute_udf, create_dummy_cube
 from openeo_r_udf.udf_lib import prepare_udf, execute_udf
-import pandas as pd
 import time
 import xarray as xr
-import numpy as np
 
 
 testfile = "./models/testing.nc"
@@ -12,17 +9,12 @@ dataset = xr.open_dataset(testfile)
 dataset = dataset.drop('transverse_mercator') # ('crs')
 data = dataset.to_array(dim = 'var')
 data = data.transpose('x','y','var')
-#data = data[0:18,0:18]
 
 
 def run(process, udf, dimension = None, context = None):
-    # Prepare data
-    print(type(data))
-    #data = create_dummy_cube(dims, sizes, labels)
 
     # Run UDF executor
     t1 = time.time() # Start benchmark
-    #result = execute_udf(process, udf, data, dimension = dimension, context = context, parallelize = parallelize, chunk_size = chunk_size)
     result = execute_udf(process, udf, data, dimension = dimension, context = context)
     t2 = time.time() # End benchmark
 
