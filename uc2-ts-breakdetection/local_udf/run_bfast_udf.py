207301edd131944bcd75022d79e2043876f0d0d6
Peter James Zellner <peterjames.zellner@eurac.edu>
2022-10-28 15:33:15 +0000

    local_udf start

--- uc2-ts-breakdetection/local_udf/run_bfast_udf.py
+++ uc2-ts-breakdetection/local_udf/run_bfast_udf.py
@@ -2,12 +2,17 @@ from openeo_r_udf.udf_lib import prepare_udf, execute_udf, save_result
 import time
 import xarray as xr
 
-# activate the environment
+## This script is mimicking what the backend would be doing when running an UDF ##
+
+# activate the environment openeo-r-udf
 
 
 # config
 load_file = "../r4openeo_uc2_ndvi_mskd_small.nc"
 save_file = "result_bfast_udf_small.nc"
+
+udf_file
+
 
 # load and prepare data
 dataset = xr.load_dataset(load_file)
@@ -17,6 +22,7 @@ data = data.squeeze('var')
 data = data.drop('var')
 data['t'] = data.t.astype('str')
 
+# define runer for udf
 def run(process, udf, dimension = None, context = None):
     # Run UDF executor
     t1 = time.time() # Start benchmark
@@ -28,9 +34,6 @@ def run(process, udf, dimension = None, context = None):
     # Print result and benchmark
     print('  Time elapsed: %s' % (t2 - t1))
     save_result(result, save_file)
-
-#print('apply')
-#run('apply', './udfs/apply.R', context = -1)
 
 print('reduce_dimension bfast')
 run('reduce_dimension', './udfs/reduce_bfast.R', dimension = 't', context = {'start_monitor': 2018})
