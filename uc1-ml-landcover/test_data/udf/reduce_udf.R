01c897d9a8206ab5ea4fd44b07ba44b95eb198c0
Basil Tufail <44399454+basu26@users.noreply.github.com>
2022-11-02 14:01:49 +0000

    Update reduce_udf.R

--- uc1-ml-landcover/test_data/udf/reduce_udf.R
+++ uc1-ml-landcover/test_data/udf/reduce_udf.R
@@ -6,13 +6,6 @@ udf = function(data,context){
   url1      <- 'https://github.com/Open-EO/r4openeo-usecases/blob/main/uc1-ml-landcover/test_data/models/TestModel1.rds?raw=True'
   getModel  <<- readRDS(gzcon(url(url1)))
   colnames(data)<-caret::predictors(getModel)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 6729a5afc4a89724aeba30fe107ae9390be5e4b6
->>>>>>> a6d6fd2cf5c6fca139deaf13571b894cb58739dc
   
 
   prediction <-predict(getModel, data)
