640d114d1b3bf8c5084fc905f40e2fd8dc5748a0
Basil Tufail <44399454+basu26@users.noreply.github.com>
2022-11-23 15:26:30 +0100

    Updates UDF

--- uc1-ml-landcover/eco4alps/udf/reduce_udf_eco4alps.R
+++ uc1-ml-landcover/eco4alps/udf/reduce_udf_eco4alps.R
@@ -6,7 +6,8 @@ udf = function(data,context){
   url1      <- 'https://github.com/Open-EO/r4openeo-usecases/blob/main/uc1-ml-landcover/eco4alps/models/TestModelIntensive_RF.rds?raw=True'
   getModel  <- readRDS(gzcon(url(url1)))
   colnames(data)<-caret::predictors(getModel)
-  
+  set.seed(321)
+
   
   prediction <-predict(getModel, data)
   prediction = as.numeric(prediction)
